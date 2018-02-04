ba5853d1572d7b25e3bf0774761e2abc398dd2f2
zhenghaishu <zhenghaishu@126.com>
2018-02-04 12:54:13 +0800

    edu37

---
+++ Education37/920A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t, n, m, a[205], i, ans;
+
+int main()
+{
+	scanf("%d", &t);
+	while(t--)
+	{
+		scanf("%d%d", &n, &m);
+
+		for(i = 1; i <= m; i++)
+        {
+            scanf("%d", a + i);
+        }
+		ans = max(a[1], n - a[m] + 1);
+
+		for(i = 1; i < m; i++)
+        {
+            // 考虑到左右两边，所以要除2，即 >> 1
+            ans = max(ans, (a[i + 1] - a[i] + 2) >> 1);
+
+        }
+        cout << ans << endl;
+
+	}
+
+	return 0;
+}
