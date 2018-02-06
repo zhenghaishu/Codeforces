abb2199c711e5072512086eabad091bb356682f5
zhenghaishu <zhenghaishu@126.com>
2018-02-06 16:20:45 +0800

    '920B'

---
+++ Education37/920B.cpp
@@ -0,0 +1,21 @@
+ #include<bits/stdc++.h>
+using namespace std;
+
+int t, n, l, r;
+int main()
+{
+	scanf("%d", &t);
+	while(t--)
+    {
+		scanf("%d", &n);
+		int res = 0;
+		for (int i = 1; i <= n; ++i)
+		{
+			scanf("%d %d", &l, &r),
+			res = max(res, l),
+			printf("%d ", res > r ? 0 : res++);
+		}
+
+		printf("\n");
+	}
+}
